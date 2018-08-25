0eed0eb0f2af6673e8b333c9bb2a43ccde9d4312
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 13:20:37 +0200

    IF

---
+++ IF/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int age;
+		age = 1;
+		
+		if(age >= 18) {
+			System.out.println("Nu får du köra bil!");
+		}
+		else {
+			System.out.println("Du får vänta några år till tyvärr");
+		}
+		
+	}
+}
