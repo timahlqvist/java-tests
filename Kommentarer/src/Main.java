ac3b70d9fca621dbc70636d20a2eba27247f6978
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 14:06:35 +0200

    comments

---
+++ Kommentarer/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// Detta är en kommentar
+		System.out.println("Hej");
+		/*
+		 * Detta är också en kommentar
+		 * men på
+		 * flera
+		 * System.out.println("rader");
+		 */
+	}
+}
