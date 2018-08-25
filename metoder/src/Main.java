b23a5db21e59423ad77039e53ce7e66880aa7265
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 23:31:49 +0200

    metoder

---
+++ metoder/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		NewClass nc = new NewClass();
+		
+		nc.newMethod();
+		
+	}
+
+}
---
+++ metoder/src/NewClass.java
@@ -0,0 +1,7 @@
+public class NewClass {
+	
+	public void newMethod() {
+		System.out.println("Denna texten ser du om du har anropat newMethod()");
+	}
+	
+}
