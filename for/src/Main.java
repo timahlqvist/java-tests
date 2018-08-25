186f9edcf99142d68774e840788d80484bf3c170
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 13:51:16 +0200

    for

---
+++ for/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x;
+		int total = 0;
+		
+		x = 5;
+		
+		for(int i = 0; i <= 10; i++) {
+			total = x * i;
+			System.out.println(x + " * " + i + " = " + total);
+		}
+	}
+}
