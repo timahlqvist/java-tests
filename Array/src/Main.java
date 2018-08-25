19784bd625a39b806db57c58fb89dca0d8a15330
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 14:12:33 +0200

    Array

---
+++ Array/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int arr[] = new int[8];
+		
+		arr[0] = 14;
+		arr[1] = 1;
+		arr[2] = 424;
+		arr[3] = 63;
+		arr[4] = 325;
+		arr[5] = 10243;
+		arr[6] = 5;
+		arr[7] = 3;
+		
+		System.out.println(arr[6]);
+		System.out.println(arr[2]);
+		System.out.println(arr[7]);
+		System.out.println(arr[0]);
+		
+	}
+}
