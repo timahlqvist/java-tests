b4577196f46b7b10d6dc9a49ead5d68b8b05a43b
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 13:57:01 +0200

    while

---
+++ while/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int y = 5;
+		int total;
+		
+		while(x <= 10) {
+			total = x * y;
+			System.out.println(x + " * " + y + " = " + total);
+			x++;
+		}
+	}
+}
