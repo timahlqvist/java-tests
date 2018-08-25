bcae652232161d6c416a39e8167470592f2b9010
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 13:13:44 +0200

    Variabler

---
+++ Variabler/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int x;
+		double y;
+		String z;
+		
+		x = 150;
+		y = 21.42;
+		z = "Hello World!";
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		
+	}
+}
