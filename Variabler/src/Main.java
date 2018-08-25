a58b43f11481e742c6a575e3c2791079fb25cc3f
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 13:16:44 +0200

    Variabler

--- Variabler/src/Main.java
+++ Variabler/src/Main.java
@@ -4,15 +4,17 @@ public class Main {
 		int x;
 		double y;
 		String z;
+		boolean timIsCool;
 		
 		x = 150;
 		y = 21.42;
 		z = "Hello World!";
+		timIsCool = true;
 		
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println(z);
-		
+		System.out.println(timIsCool);
 		
 	}
 }
