c8db895ce47119416dd7f95e671f5077110d359b
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 14:14:36 +0200

    Array

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int arr[] = new int[8];
+		int arr2[] = {1, 53, 7432, 2346, 23, 0, 1, 4, 51};
 		
 		arr[0] = 14;
 		arr[1] = 1;
@@ -17,6 +18,11 @@ public class Main {
 		System.out.println(arr[2]);
 		System.out.println(arr[7]);
 		System.out.println(arr[0]);
+		
+		System.out.println(arr2[0]);
+		System.out.println(arr2[4]);
+		System.out.println(arr2[2]);
+		System.out.println(arr2[7]);
 		
 	}
 }
