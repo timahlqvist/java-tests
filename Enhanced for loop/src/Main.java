9622e534a97ace815ca94dd6846912f35322ad3a
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-25 14:20:37 +0200

    Enhanced for loop

---
+++ Enhanced for loop/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[3];
+		
+		arr[0] = 23;
+		arr[1] = 11;
+		arr[2] = 63;
+		
+		for(int x : arr) {
+			System.out.println("Värdet i arrayen är " + x);
+		}
+		
+	}
+}
