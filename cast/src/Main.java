d863cbaec9fae663f05a794ba32e81de88332dd2
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-26 13:11:56 +0200

    cast

---
+++ cast/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double x;
+		int y;
+		
+		x = 13.64;
+		y = 2;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		x = (int) y;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
