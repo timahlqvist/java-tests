a23a2450e4ee13f4ab1436e67147dc5956bbcc98
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-26 11:39:49 +0200

    in

---
+++ Input/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {	
+		Scanner scan = new Scanner(System.in);
+		
+		int x;
+		String y;
+		
+		/*System.out.println("Skriv en siffra: ");
+		x = scan.nextInt();
+		
+		System.out.println("Du skrev siffran " + x);
+		*/
+		
+		System.out.println("Skriv en liten fin textbit ur en bra låt: ");
+		y = scan.nextLine();
+		System.out.println("Du skrev: " + y);
+		
+	}
+
+}
