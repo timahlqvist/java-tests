45565945da655a5574ce9059daf698246c5d09d5
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-26 10:51:34 +0200

    Accessors

---
+++ Accessors/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		NewClass nc = new NewClass();
+		
+		nc.setX(143);
+		System.out.println(nc.getX());
+		
+	}
+}
---
+++ Accessors/src/NewClass.java
@@ -0,0 +1,12 @@
+public class NewClass {
+
+	private int x;
+	
+	protected void setX(int x) {
+		this.x = x;
+	}
+	protected int getX() {
+		return x;
+	}
+	
+}
