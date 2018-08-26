76476dc05143f7ac7373b1e6b51350a5a319d819
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-26 13:26:19 +0200

    MO

---
+++ Method Overloading/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Math m = new Math();
+		
+		System.out.println(m.area(15, 12.25));
+		
+	}
+
+}
---
+++ Method Overloading/src/Math.java
@@ -0,0 +1,10 @@
+public class Math {
+	
+	public int area(int b, int h) {
+		return b * h;
+	}
+	public double area(double b, double h) {
+		return b * h;
+	}
+	
+}
