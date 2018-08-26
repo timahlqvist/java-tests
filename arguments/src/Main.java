a1e8d6b8e58caae4c25d57afa412d126ca6dc025
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-26 10:43:17 +0200

    argument eller parametrar

---
+++ arguments/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		NewClass nc = new NewClass();
+		
+		nc.math(5, 10);
+
+	}
+
+}
---
+++ arguments/src/NewClass.java
@@ -0,0 +1,12 @@
+public class NewClass {
+
+	protected static void math(int arg1, int arg2) {
+		
+		int total;
+		total = arg1 * arg2;
+		
+		System.out.println(arg1 + " * " + arg2 + " = " + total);
+		
+	}
+	
+}
