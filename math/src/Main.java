bea392af2477ce037c70dedc2044683adc4a04ce
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-26 11:17:41 +0200

    math

---
+++ math/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double x;
+		double calc;
+		double calc2;
+		
+		x = 32;
+		calc = Math.sqrt(x);
+		calc2 = Math.PI;
+		
+		System.out.println(calc);
+		System.out.println(calc2);
+		
+	}
+
+}
